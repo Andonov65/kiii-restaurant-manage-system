fa87c5dcf8af08171f8eb3856eaac91ea3e63e33
Marko Andonov <marko.20112000@gmail.com>
2022-03-05 17:30:51 +0100

    service commit

--- src/main/java/com/finki/wp/ugostitelskiobjekti/Service/UgostitelskiObjektService.java
+++ src/main/java/com/finki/wp/ugostitelskiobjekti/Service/UgostitelskiObjektService.java
@@ -9,22 +9,19 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 public interface UgostitelskiObjektService {
-
-    UgostitelskiObjekt save(String ime, String adresa, String opis, String slika, Integer vkupnoMasi, String grad, String shef);
     List<UgostitelskiObjekt> findAll();
     UgostitelskiObjekt findById(Long id);
-    public UgostitelskiObjekt rezerviraj(Long id);
+     UgostitelskiObjekt rezerviraj(Long id);
     void setPhotos(String fileName);
 
     void saveObj(String ime, String adresa, String opis, MultipartFile slika, Integer vkupnoMasi, String grad, String shef);
 
     List<UgostitelskiObjekt> findAllByShefUserName(String username);
 
-
     List<UgostitelskiObjekt> findAllByShefUserName(Shef shef) ;
 
     List<List<Vraboten>> findAllEmployeesByShef(Shef shef);
     List<Vraboten> findAllEmployeesByShef2(Shef shef);
 
-        Vraboten vraboti(String username,Long objId);
+    Vraboten vraboti(String username,Long objId);
 }
